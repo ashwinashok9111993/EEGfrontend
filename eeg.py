9a82e2fbe57454de9914ab0aa8a95a90b16febcf
ashwinashok9111993 <ashwinashok9111993@gmail.com>
2013-03-24 18:51:48 +0530

    Create eeg.py

---
+++ eeg.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as mp
+import math as m
+import scipy.io as sc
+import scipy.stats as st
+from time import *
+from xlwt.Workbook import *
+from xlwt.Style import *
+from array import *
+from Tkinter import *
+from ttk import Frame, Label, Scale, Style
+
+
+
+class GUI(Frame):
+    def __init__(self,parent):
+        Frame.__init__(self,parent)
+        self.parent=parent
+        self.initUI()
+
+    def loadmat(self):
+        print 'ok'
+        
+    def onExit(self):
+  	self.quit()
+
+    def initUI(self):
+		self.parent.title("EEGanalyser")
+		menubar=Menu(self.parent)
+		self.parent.config(menu=menubar)
+		fileMenu = Menu(menubar)
+		fileMenu.add_command(label="load .mat",command = self.loadmat)
+		fileMenu.add_command(label="Exit", command=self.onExit)
+		menubar.add_cascade(label="File", menu=fileMenu)
+			
+       
+def main():
+    root = Tk() 
+    app=GUI(root)
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
+    
+
